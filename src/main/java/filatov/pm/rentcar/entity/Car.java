eb7fff4c256821a4a1ec9fd00a24200a8f42a3ca
Andrey <filatovprivate@gmail.com>
2022-04-29 17:46:04 +0800

    add Car in model

--- src/main/java/filatov/pm/rentcar/entity/Car.java
+++ src/main/java/filatov/pm/rentcar/entity/Car.java
@@ -1,9 +1,18 @@
 package filatov.pm.rentcar.entity;
 
-import lombok.Data;
+import lombok.*;
 
-@Data
+import javax.persistence.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="cars")
 public class Car {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String model;
     private String mark;
