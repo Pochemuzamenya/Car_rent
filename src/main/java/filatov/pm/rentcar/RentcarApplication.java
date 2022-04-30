ff882f77ebe175806d9b91f129097efc7a1e3695
Andrey <filatovprivate@gmail.com>
2022-04-30 16:18:10 +0800

    Feat: add Bean sessionFactory

--- src/main/java/filatov/pm/rentcar/RentcarApplication.java
+++ src/main/java/filatov/pm/rentcar/RentcarApplication.java
@@ -1,7 +1,14 @@
 package filatov.pm.rentcar;
 
+import org.hibernate.reactive.mutiny.Mutiny;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 @SpringBootApplication
 public class RentcarApplication {
@@ -10,4 +17,9 @@ public class RentcarApplication {
 		SpringApplication.run(RentcarApplication.class, args);
 	}
 
+	@Bean(name = "sessionFactory")
+	public Mutiny.SessionFactory getSession(){
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("factory");
+		return emf.unwrap(Mutiny.SessionFactory.class);
+	}
 }
