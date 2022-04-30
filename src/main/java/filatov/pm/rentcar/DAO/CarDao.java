ba8a5a0ed7e403012285834caa054c38edfd1269
Andrey <filatovprivate@gmail.com>
2022-04-30 18:58:16 +0800

    Feat: add car dao

---
+++ src/main/java/filatov/pm/rentcar/DAO/CarDao.java
@@ -0,0 +1,86 @@
+package filatov.pm.rentcar.DAO;
+
+import filatov.pm.rentcar.entity.Car;
+import lombok.AllArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.hibernate.reactive.mutiny.Mutiny;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+@AllArgsConstructor(onConstructor = @__(@Autowired))
+public class CarDao implements Dao<Car> {
+
+    private static final Logger log = LogManager.getLogger();
+
+    Mutiny.SessionFactory sessionFactory;
+
+    @Override
+    public void save(Car car) {
+        sessionFactory.withTransaction(
+                        session -> session.persist(car))
+                .await().indefinitely();
+        log.trace(car + " saved");
+    }
+
+    @Override
+    public Optional<Car> findById(Integer id) {
+        return Optional.ofNullable(sessionFactory.withTransaction(
+                session -> session.createQuery("FROM Car c WHERE c.id = :id", Car.class)
+                        .setParameter("id", id)
+                        .getSingleResultOrNull()
+        ).await().indefinitely());
+    }
+
+    @Override
+    public Optional<Car> findByName(String name) {
+        return Optional.ofNullable(sessionFactory.withTransaction(
+                session -> session.createQuery("FROM Car c WHERE c.model =:name", Car.class)
+                        .setParameter("name", name)
+                        .getSingleResultOrNull()
+        ).await().indefinitely());
+    }
+
+    @Override
+    public List<Car> findAll() {
+        return sessionFactory.withTransaction(
+                        session -> session.createQuery("SELECT c FROM Car c", Car.class)
+                                .getResultList())
+                .await().indefinitely();
+    }
+
+    @Override
+    public void update(Car car, Car e) {
+        sessionFactory.withTransaction(
+                session -> session.find(Car.class, car.getId())
+                        .invoke(c -> {
+                            c.setCarBody(e.getCarBody());
+                            c.setMark(e.getMark());
+                            c.setModel(e.getModel());
+                            c.setReleaseDate(e.getReleaseDate());
+                            c.setStatus(e.getStatus());
+                            c.setRentalPrice(e.getRentalPrice());
+                        })
+        ).await().indefinitely();
+    }
+
+    @Override
+    public void delete(Integer id) {
+        sessionFactory.withTransaction(
+                session -> session.find(Car.class, id)
+                        .chain(session::remove)
+        ).await().indefinitely();
+        log.trace("deleted");
+    }
+
+    @Override
+    public void deleteAll() {
+        sessionFactory.withTransaction(
+                        (session, tx) -> session.createQuery("delete Car").executeUpdate())
+                .await().indefinitely();
+    }
+}
---
+++ src/main/java/filatov/pm/rentcar/DAO/CarDaoImpl.java
@@ -0,0 +1,51 @@
+package filatov.pm.rentcar.DAO;
+
+import filatov.pm.rentcar.entity.Car;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+@AllArgsConstructor(onConstructor = @__(@Autowired))
+public class CarDaoImpl implements Dao<Car>{
+
+    Dao<Car> carDao;
+
+    @Override
+    public void save(Car car) {
+        carDao.save(car);
+    }
+
+    @Override
+    public Optional<Car> findById(Integer id) {
+        return carDao.findById(id);
+    }
+
+    @Override
+    public Optional<Car> findByName(String name) {
+        return carDao.findByName(name);
+    }
+
+    @Override
+    public List<Car> findAll() {
+        return carDao.findAll();
+    }
+
+    @Override
+    public void update(Car car, Car e) {
+        carDao.update(car, e);
+    }
+
+    @Override
+    public void delete(Integer id) {
+        carDao.delete(id);
+    }
+
+    @Override
+    public void deleteAll() {
+        carDao.deleteAll();
+    }
+}
