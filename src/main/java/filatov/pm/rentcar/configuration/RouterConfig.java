4eb6a521261c611f21b08228b02d030a50c27811
Andrey <filatovprivate@gmail.com>
2022-06-11 19:07:29 +0800

    Feat: add api for customers
    add GET, POST, DELETE requests

--- src/main/java/filatov/pm/rentcar/configuration/RouterConfig.java
+++ src/main/java/filatov/pm/rentcar/configuration/RouterConfig.java
@@ -1,5 +1,6 @@
 package filatov.pm.rentcar.configuration;
 
+import filatov.pm.rentcar.handlers.CustomerHandler;
 import filatov.pm.rentcar.handlers.EmployeeHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,6 +18,13 @@ public class RouterConfig {
     RouterFunction<ServerResponse> routes(EmployeeHandler handler) {
         return route(GET("/handler/staff").and(accept(MediaType.APPLICATION_JSON)), handler::getAll)
                 .andRoute(POST("/handler/staff").and(accept(MediaType.APPLICATION_JSON)), handler::create)
-                .andRoute(DELETE("handler/staff/{userId}").and(accept(MediaType.APPLICATION_JSON)), handler::delete);
+                .andRoute(DELETE("/handler/staff/{id}").and(accept(MediaType.APPLICATION_JSON)), handler::delete);
+    }
+
+    @Bean
+    RouterFunction<ServerResponse> router(CustomerHandler handler) {
+        return route(GET("/handler/customers").and(accept(MediaType.APPLICATION_JSON)), handler::getAll)
+                .andRoute(POST("/handler/customers").and(accept(MediaType.APPLICATION_JSON)) , handler::create)
+                .andRoute(DELETE("handler/customers/{id}").and(accept(MediaType.APPLICATION_JSON)), handler::delete);
     }
 }
