79365fb3518128ee907b4093d154d12ecb0f5e68
Andrey <filatovprivate@gmail.com>
2022-06-17 21:21:05 +0800

    Feat: add routers for car and order entity

--- src/main/java/filatov/pm/rentcar/configuration/RouterConfig.java
+++ src/main/java/filatov/pm/rentcar/configuration/RouterConfig.java
@@ -1,7 +1,9 @@
 package filatov.pm.rentcar.configuration;
 
+import filatov.pm.rentcar.handlers.CarHandler;
 import filatov.pm.rentcar.handlers.CustomerHandler;
 import filatov.pm.rentcar.handlers.EmployeeHandler;
+import filatov.pm.rentcar.handlers.OrderHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
@@ -15,16 +17,30 @@ import static org.springframework.web.reactive.function.server.RouterFunctions.r
 public class RouterConfig {
 
     @Bean
-    RouterFunction<ServerResponse> routes(EmployeeHandler handler) {
+    RouterFunction<ServerResponse> staffRouter(EmployeeHandler handler) {
         return route(GET("/handler/staff").and(accept(MediaType.APPLICATION_JSON)), handler::getAll)
                 .andRoute(POST("/handler/staff").and(accept(MediaType.APPLICATION_JSON)), handler::create)
                 .andRoute(DELETE("/handler/staff/{id}").and(accept(MediaType.APPLICATION_JSON)), handler::delete);
     }
 
     @Bean
-    RouterFunction<ServerResponse> router(CustomerHandler handler) {
+    RouterFunction<ServerResponse> customerRouter(CustomerHandler handler) {
         return route(GET("/handler/customers").and(accept(MediaType.APPLICATION_JSON)), handler::getAll)
                 .andRoute(POST("/handler/customers").and(accept(MediaType.APPLICATION_JSON)) , handler::create)
-                .andRoute(DELETE("handler/customers/{id}").and(accept(MediaType.APPLICATION_JSON)), handler::delete);
+                .andRoute(DELETE("/handler/customers/{id}").and(accept(MediaType.APPLICATION_JSON)), handler::delete);
+    }
+
+    @Bean
+    RouterFunction<ServerResponse> carRouter(CarHandler handler) {
+        return route(GET("/handler/cars").and(accept(MediaType.APPLICATION_JSON)), handler::getAll)
+                .andRoute(POST("/handler/cars").and(accept(MediaType.APPLICATION_JSON)) , handler::create)
+                .andRoute(DELETE("/handler/cars/{id}").and(accept(MediaType.APPLICATION_JSON)), handler::delete);
+    }
+
+    @Bean
+    RouterFunction<ServerResponse> orderRouter(OrderHandler handler) {
+        return route(GET("/handler/orders").and(accept(MediaType.APPLICATION_JSON)), handler::getAll)
+                .andRoute(POST("/handler/orders").and(accept(MediaType.APPLICATION_JSON)) , handler::create)
+                .andRoute(DELETE("/handler/orders/{id}").and(accept(MediaType.APPLICATION_JSON)), handler::delete);
     }
 }
