80f664c2f5c90c3676446830ca3730aa2399f6b5
Andrey <filatovprivate@gmail.com>
2022-06-17 21:17:43 +0800

    Test: add test findAll and findById, correct test save

--- src/test/java/filatov/pm/rentcar/DAO/dao/OrderDaoTest.java
+++ src/test/java/filatov/pm/rentcar/DAO/dao/OrderDaoTest.java
@@ -9,14 +9,9 @@ import org.junit.Test;
 
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.Month;
-import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
 import java.util.Set;
-
-import static org.junit.Assert.*;
 
 public class OrderDaoTest {
     private static final Logger log = LogManager.getLogger();
@@ -27,6 +22,7 @@ public class OrderDaoTest {
     Dao<Employee> empService = new EmployeeDao(emf.unwrap(Mutiny.SessionFactory.class));
     Dao<Car> carService = new CarDao(emf.unwrap(Mutiny.SessionFactory.class));
     Dao<Customer> customerService = new CustomerDao(emf.unwrap(Mutiny.SessionFactory.class));
+    Dao<Branch> branchService = new BranchDao(emf.unwrap(Mutiny.SessionFactory.class));
 
     Car car = new Car();
     Customer customer = new Customer();
@@ -35,7 +31,44 @@ public class OrderDaoTest {
 
     @Before
     public void setUp() throws Exception {
+        car.setMark("Test mark");
+        car.setModel("Test model");
+        car.setCarBody("Test car body");
+        car.setStatus(Status.OK);
+        car.setRentalPrice(200D);
+        car.setReleaseDate(1993);
+        carService.save(car);
 
+        customer.setBranches(Set.of());
+        customer.setOrders(Set.of());
+        customer.setState(CustomerState.OK);
+        customer.setName("Test Name");
+        customerService.save(customer);
+
+        employee.setPassword("test password");
+        employee.setLogin("test login");
+        employee.setName("test name");
+        empService.save(employee);
+        Car car1 = carService.findById(1).get();
+        //Customer customer1 = customerService.findById(1).get();
+        Employee employee1 = empService.findById(1).get();
+        log.info(car1);
+        log.info(customer);
+        log.info(employee1);
+
+        Branch branch = new Branch();
+        branchService.save(branch);
+
+        Order order1 = new Order(car1, employee1, customer ,branch, employee1);
+        order1.setupGaveOutDate();
+        order1.setupRefundDate();
+        //order1.setupFactRefundDate();
+        order1.calculateDeposit();
+        order1.setupTitle();
+        log.info(order1);
+
+        orderService.save(order1);
+        log.info(order1.getTitle() + " saved");
     }
 
     @Test
@@ -59,21 +92,31 @@ public class OrderDaoTest {
         employee.setName("test name");
         empService.save(employee);
         Car car1 = carService.findById(1).get();
-        Customer customer1 = customerService.findById(1).get();
+        //Customer customer1 = customerService.findById(1).get();
         Employee employee1 = empService.findById(1).get();
         log.info(car1);
-        log.info(customer1);
+        log.info(customer);
         log.info(employee1);
 
-        Order order1 = new Order(car1, employee1,customer1,new Branch(), employee1);
+        Branch branch = new Branch();
+        branchService.save(branch);
+
+        Order order1 = new Order(car1, employee1, customer ,branch, employee1);
+        order1.setupGaveOutDate();
+        order1.setupRefundDate();
+        //order1.setupFactRefundDate();
+        order1.calculateDeposit();
+        order1.setupTitle();
         log.info(order1);
-//
-//        orderService.save(order1);
-//        log.info(order1 + " saved");
+
+        orderService.save(order1);
+        log.info(order1.getTitle() + " saved");
     }
 
     @Test
     public void findById() {
+        Optional<Order> byId = orderService.findById(1);
+        log.info(byId.get());
     }
 
     @Test
@@ -82,6 +125,8 @@ public class OrderDaoTest {
 
     @Test
     public void findAll() {
+        List<Order> all = orderService.findAll();
+        all.forEach(System.out::println);
     }
 
     @Test
