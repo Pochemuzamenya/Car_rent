d81233b577c2af870fb3a76b73746dcdfb215862
Andrey <filatovprivate@gmail.com>
2022-06-27 23:31:25 +0800

    Fix: refactor

--- src/main/java/filatov/pm/rentcar/DAO/serviceImpl/BranchService.java
+++ src/main/java/filatov/pm/rentcar/DAO/serviceImpl/BranchService.java
@@ -1,7 +1,7 @@
 package filatov.pm.rentcar.DAO.serviceImpl;
 
 import filatov.pm.rentcar.DAO.dao.Dao;
-import filatov.pm.rentcar.entity.Branch;
+import filatov.pm.rentcar.entity.Branch.Branch;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
